70b57119e886f22b9b7f80fa507fdba94ddebaba
lathoub <4082369+lathoub@users.noreply.github.com>
2024-01-09 15:55:28 +0100

    Update index.js

--- step2/index.js
+++ step2/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World, step 2!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
